b52b53fbe5dc899a05b4b76949b16dcb10067706
MOHAMED OUAICHA <ouaicha47@gmail.com>
2024-09-07 18:10:18 +0100

    Fixes #2: Format the code

--- src/main_reasoning.py
+++ src/main_reasoning.py
@@ -1,30 +1,37 @@
-from flask import Flask, request, jsonify
+import json
+
 import dotenv
 import streamlit as st
-import json
-from models.functions import get_resume_details, resume_details_builder, get_job_details, get_matching_score
-    
+from flask import Flask, jsonify, request
+
+from models.functions import (
+    get_job_details,
+    get_matching_score,
+    get_resume_details,
+    resume_details_builder,
+)
+
 dotenv.load_dotenv()
 
 app = Flask(__name__)
 
-@app.route('/query', methods=['POST'])
+
+@app.route("/query", methods=["POST"])
 def query():
     data = request.json
-    job_description = data.get('job_description')
-    resume_text = data.get('resume')
+    job_description = data.get("job_description")
+    resume_text = data.get("resume")
 
-    
     if not job_description:
         print("job is empty am3llem")
         return jsonify({"error": "No query provided"}), 400
-    
+
     if not resume_text:
         print("resume is empty am3llem")
         return jsonify({"error": "No resume provided"}), 400
-    
+
     # this one is the text that will be used to query the model, the TEXT_GENERATION_PROMPT is a template that will be filled with the job description and the resume
-    
+
     response_text_resume = get_resume_details(resume_text)
     response_text_job = get_job_details(job_description)
     response_text = response_text_resume + response_text_job
@@ -32,7 +39,9 @@ def query():
     scores = get_matching_score(response_text, resume_text)
     scores_json = json.loads(scores)
     if scores_json["Match Percentage"] > 60:
-        st.write(f"\n Your resume matches the job description with a score of {scores_json['Match Percentage']} %")
+        st.write(
+            f"\n Your resume matches the job description with a score of {scores_json['Match Percentage']} %"
+        )
         # response_text = get_questions(response_text)
         # response_text = get_answers(response_text)
         # response_text = get_questions_score(response_text)
@@ -45,5 +54,6 @@ def query():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
